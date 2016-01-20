ea44cbf61008b17f20b23f15b0cc0217eab53e61
Ryan Wolf <ryanwolf@ufl.edu>
2016-01-20 14:42:52 -0500

    First Commit

---
+++ UF-Directory-App-Assignment-Assignment3/JSONtoMongo.js
@@ -0,0 +1,35 @@
+'use strict';
+/* 
+  Import modules/files you may need to correctly run the script. 
+  Make sure to save your DB's uri in the config file, then import it with a require statement!
+ */
+var fs = require('fs'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config.js');
+
+
+/* Connect to your database */
+mongoose.connect(config.db.uri);
+/* 
+  Instantiate a mongoose model for each listing object in the JSON file, 
+  and then save it to your Mongo database 
+ */
+
+var data = fs.readFileSync('listings.json').toString();
+var listingData = JSON.parse(data);
+
+for(var i=0; i<listingData.entries.length; i++) {
+    var newEntry = new Listing(listingData.entries[i]);
+    console.log(newEntry);
+    newEntry.save(function(err) {
+        if (err) throw err;
+        console.log('entry added');
+    });
+};
+
+/* 
+  Once you've written + run the script, check out your MongoLab database to ensure that 
+  it saved everything correctly. 
+ */
